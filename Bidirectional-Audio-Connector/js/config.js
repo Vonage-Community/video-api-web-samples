c2931a93d266dc52bb82a2b98f191c92558289ba
Dwane Hemmings <dwane.hemmings@vonage.com>
2025-09-02 13:04:30 -0400

    Remove unused configuration variables

--- Bidirectional-Audio-Connector/js/config.js
+++ Bidirectional-Audio-Connector/js/config.js
@@ -5,11 +5,3 @@
 // Do not include the trailing slash. See the README for more information:
 
 const SAMPLE_SERVER_BASE_URL = 'https://YOUR-SERVER-URL';
-
-// OR, if you have not set up a web server that runs the sample-video-php-learning_server code,
-// set these values to Vonage Application ID, a valid session ID, and a token for the session.
-// For test purposes, you can obtain these from https://tools.vonage.com/video/playground.
-
-const APPLICATION_ID = null;
-const SESSION_ID = null;
-const TOKEN = null;
